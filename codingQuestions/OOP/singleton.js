a4646949dbbc2c95d05fbcd0f21473bb9bb0d6e2
Nolawee Mengist <nolawee.mengist@omnicommediagroup.com>
2022-02-12 11:10:46 -0500

    singleton practice

--- codingQuestions/OOP/singleton.js
+++ codingQuestions/OOP/singleton.js
@@ -1,76 +1,101 @@
-// class Settings {
-//     constructor() {
-//         if (Settings.instance instanceof Settings) {
-//             return Settings.instance;
-//         }
+// // class Settings {
+// //     constructor() {
+// //         if (Settings.instance instanceof Settings) {
+// //             return Settings.instance;
+// //         }
 
-//         this.settingsObject = {
-//             'background': '#ff0000',
-//             'version': Math.floor(Math.random() * 4000)
-//         };
+// //         this.settingsObject = {
+// //             'background': '#ff0000',
+// //             'version': Math.floor(Math.random() * 4000)
+// //         };
 
-//         Object.freeze(this.settingsObject);
-//         // people can't modify the instance we create
-//         Object.freeze(this);
-//         Settings.instance = this;
-//     }
+// //         Object.freeze(this.settingsObject);
+// //         // people can't modify the instance we create
+// //         Object.freeze(this);
+// //         Settings.instance = this;
+// //     }
 
-//     get(key) {
-//         return this.settingsObject[key];
-//     }
-// }
+// //     get(key) {
+// //         return this.settingsObject[key];
+// //     }
+// // }
 
-// const s = new Settings();
-// const a = new Settings()
+// // const s = new Settings();
+// // const a = new Settings()
 
-// console.log(s === a)
+// // console.log(s === a)
+
+
+// // class Singleton {
+// //     constructor() {
+
+// //         if(Singleton.instance instanceof Singleton) {
+// //             return Singleton.instance;
+// //         }
+        
+// //         this.singletonObject = {
+// //             name: "singleton instance",
+// //             version: Math.floor(Math.random()* 4000)
+// //         }
+
+// //         Object.freeze(this.singletonObject);
+// //         Object.freeze(this);
+
+// //     }
+
+// //     get(key) {
+// //         return this.singletonObject[key];
+// //     }
+// // }
 
 
 // class Singleton {
-//     constructor() {
 
-//         if(Singleton.instance instanceof Singleton) {
-//             return Singleton.instance;
-//         }
-        
+//     constructor() {
+//         if(Singleton.instance instanceof Singleton) return Singleton.instance;
+
 //         this.singletonObject = {
-//             name: "singleton instance",
-//             version: Math.floor(Math.random()* 4000)
+//             name: 'singletonExample',
+//             version: Math.floor(Math.random*5000)
 //         }
 
 //         Object.freeze(this.singletonObject);
 //         Object.freeze(this);
-
+//         Singleton.instance = this;
 //     }
 
-//     get(key) {
-//         return this.singletonObject[key];
+//     get(key){
+//         return this.singletonObject[key]
 //     }
 // }
 
-
-class Singleton {
+class Singleton { 
 
     constructor() {
-        if(Singleton.instance instanceof Singleton) return Singleton.instance;
+        if(Singleton.instance instanceof Singleton) return Singleton.instance
+
 
         this.singletonObject = {
-            name: 'singletonExample',
-            version: Math.floor(Math.random*5000)
-        }
+            name: 'singleton1',
+            version: Math.floor(Math.random() * 4000)
+        };
 
         Object.freeze(this.singletonObject);
         Object.freeze(this);
         Singleton.instance = this;
+
     }
 
-    get(key){
-        return this.singletonObject[key]
+    get(key) {
+        return this.singletonObject[key];
     }
+    
 }
-
 
 const a = new Singleton();
 const b = new Singleton();
 
 console.log(a === b);
+
+console.log('a version ' + a.get('version'))
+console.log('b version ' + b.get('version'))
